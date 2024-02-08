44ea65a60fae06db1b2f5969f48c0c65e8d70e33
Andre Ferreira <andreferreira177@gmail.com>
2024-02-08 18:03:31 -0300

    adding cors

--- api.py
+++ api.py
@@ -4,10 +4,14 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 @app.route('/ping', methods=['GET'])
 def ping():
-    return jsonify({'response': 'pong'})
+    response = jsonify({'response': 'pong'})
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
 @app.route('/health', methods=['GET'])
 def healthcheck():
-    return jsonify({'status': 'ok'})
+    response = jsonify({'status': 'ok'})
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
 app.run(host='0.0.0.0', port=5000)
